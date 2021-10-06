70c6f2dac1a7a6e9693e2664284754ed1d2d84b2
Breno Santos <brenosantos145@gmail.com>
2021-10-06 18:21:35 -0300

    update

---
+++ fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
@@ -0,0 +1,9 @@
+const myName = "Breno Santos";
+const birthCity = "São Paulo";
+let birthYear = 2000;
+
+birthYear = 2030;
+// birthCity = "Diadema";
+// Uma Variavel do tipo "const" é constante, ou seja, não pode ser alterada
+
+// console.log(birthCity);
