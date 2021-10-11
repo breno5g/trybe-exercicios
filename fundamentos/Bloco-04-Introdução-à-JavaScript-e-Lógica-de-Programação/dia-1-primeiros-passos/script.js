6bef7ec77ab30488ba5ecdd9937f8b00db900d5f
Breno Santos <brenosantos145@gmail.com>
2021-10-11 08:33:17 -0300

    ex005

--- fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
+++ fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
@@ -36,6 +36,26 @@
 
 // Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
-const n1 = 10;
+// const n1 = 10;
 
-console.log(n1 >= 0 ? 'positive' : 'negative');
+// console.log(n1 >= 0 ? 'positive' : 'negative');
+
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+let a = 65;
+let b = 100;
+let c = 15;
+
+let somaDosAngulos = a + b + c;
+
+let positivoEOperante = a > 0 && b > 0 && c > 0;
+
+if (positivoEOperante) {
+  if (somaDosAngulos === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+} else {
+  console.log('Erro: ângulo inválido');
+}
