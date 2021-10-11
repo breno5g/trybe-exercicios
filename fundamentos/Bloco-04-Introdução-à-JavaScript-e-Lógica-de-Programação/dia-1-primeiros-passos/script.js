7d8401f08cbdf4e889160e6cbdf70e2da9db2115
Breno Santos <brenosantos145@gmail.com>
2021-10-11 08:22:52 -0300

    ex002

--- fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
+++ fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
@@ -1,10 +1,21 @@
 // Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 
-const a = 10;
-const b = 2;
+// const a = 10;
+// const b = 2;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 10;
+const b = 13;
+
+if (a > b) {
+  console.log(`O maior numero é ${a}`);
+} else {
+  console.log(`O maior numero é ${b}`);
+}
