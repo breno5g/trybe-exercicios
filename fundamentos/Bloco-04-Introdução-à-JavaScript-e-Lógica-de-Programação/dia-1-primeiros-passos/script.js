1a87f458f8d3b0f9a23da996ef5450ad5ebf686c
Breno Santos <brenosantos145@gmail.com>
2021-10-11 08:39:05 -0300

    ex006

--- fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
+++ fundamentos/Bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-1-primeiros-passos/script.js
@@ -38,24 +38,51 @@
 
 // const n1 = 10;
 
-// console.log(n1 >= 0 ? 'positive' : 'negative');
+// console.log(n1 >= 0 ? "positive" : "negative");
 
 // Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
-let a = 65;
-let b = 100;
-let c = 15;
+// let a = 65;
+// let b = 100;
+// let c = 15;
 
-let somaDosAngulos = a + b + c;
+// let somaDosAngulos = a + b + c;
 
-let positivoEOperante = a > 0 && b > 0 && c > 0;
+// let positivoEOperante = a > 0 && b > 0 && c > 0;
 
-if (positivoEOperante) {
-  if (somaDosAngulos === 180) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
-} else {
-  console.log('Erro: ângulo inválido');
+// if (positivoEOperante) {
+//   if (somaDosAngulos === 180) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// } else {
+//   console.log("Erro: ângulo inválido");
+// }
+
+// Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+const peca = 'rainha';
+
+switch (peca) {
+  case 'peão':
+    console.log('Uma casa em linha reta ou duas na primeira movimentação');
+    break;
+  case 'torre':
+    console.log('Quantidade indefinida de casa no eixo x e y');
+    break;
+  case 'bispo':
+    console.log('Quantidade indefinida de casa no eixo x/y');
+    break;
+  case 'cavalo':
+    console.log('Movimento em L');
+    break;
+  case 'rei':
+    console.log('Um movimento para qualquer direção');
+    break;
+  case 'rainha':
+    console.log('Quantidade indefinida de movimentos para qualquer eixo');
+    break;
+  default:
+    console.log('Por favor insira uma peça valida');
 }
