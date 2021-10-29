1cd7e8cf8f6a5101e584e7435ca08327caa498c9
Breno Santos <brenosantos145@gmail.com>
2021-10-29 16:09:40 -0300

    form feio

--- fundamentos/Bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-1-html-css-forms/teste 4/script.js
+++ fundamentos/Bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-1-html-css-forms/teste 4/script.js
@@ -10,7 +10,74 @@ fetch(URL)
       let opt = document.createElement('option');
       opt.value = e.sigla;
       opt.text = e.nome;
-      console.log(opt);
       document.querySelector('#input-state').appendChild(opt);
     });
   });
+
+let date = document.querySelector('#initial-date');
+
+date.addEventListener('keyup', (e) => {
+  if (Number(e.target.value) === NaN) {
+    return false;
+  }
+  if (e.key !== 'Backspace') {
+    if (e.target.value.length === 2) {
+      e.target.value += '/';
+    }
+    if (e.target.value.length === 5) {
+      e.target.value += '/';
+    }
+  }
+});
+
+function dateFormater() {
+  let spaces = date.value.split('/');
+  for (let cel of spaces) {
+    if (isNaN(Number(cel)) || Math.sign(Number(cel)) === -1) {
+      return false;
+    }
+  }
+  if (spaces[0] <= 0 || spaces[0] > 31) {
+    alert('Dia inserido invalido');
+  }
+  if (spaces[1] <= 0 || spaces[1] > 12) {
+    alert('Mês inserido invalido');
+  }
+  if (Math.sign(Number(spaces[2]))) {
+    alert('Ano inserido invalido');
+  }
+}
+
+document
+  .querySelector('button[type="submit"]')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    mountCurriculum();
+  });
+
+let classes = [
+  'personName',
+  'email',
+  'cpf',
+  'adress',
+  'adress-type',
+  'resume',
+  'charge',
+  'date',
+];
+
+function mountCurriculum() {
+  const formData = new FormData(document.querySelector('form'));
+  document.querySelector('.personName').textContent =
+    formData.get('input-name');
+  document.querySelector('.email').textContent = formData.get('input-email');
+  document.querySelector('.cpf').textContent = formData.get('input-cpf');
+  document.querySelector('.adress').textContent = formData.get('input-adress');
+  document.querySelector('.state').textContent = formData.get('input-state');
+  document.querySelector('.adress-type').textContent =
+    formData.get('radio-tipo');
+  document.querySelector('.charge').textContent = formData.get('input-charge');
+  document.querySelector('.resume').textContent =
+    formData.get('charge-description');
+  document.querySelector('.date').textContent = formData.get('input-date');
+}
