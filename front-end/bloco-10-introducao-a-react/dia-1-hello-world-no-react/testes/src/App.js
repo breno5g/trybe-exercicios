68260495664409c2a400e581bf2c072e0aaa6e8f
Breno Santos <brenosantos145@gmail.com>
2021-11-21 19:02:59 -0300

    teste

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testes/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// Componenentes começam com letra maiscula
+import TesteComponente from './component';
+
+function App() {
+  return <TesteComponente />;
+}
+
+export default App;
