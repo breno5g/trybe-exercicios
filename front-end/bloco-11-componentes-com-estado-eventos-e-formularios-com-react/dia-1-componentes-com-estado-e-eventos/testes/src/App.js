b8e04b715c90f13263449d0d19776679a3cb7738
Breno Santos <brenosantos145@gmail.com>
2021-11-27 09:17:04 -0300

    update

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/testes/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import FirstEx from './components/ex001';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstEx />
+    </div>
+  );
+}
+
+export default App;
