ef95b1b457eacd3c090ab67da421d22de86ef599
Breno Santos <brenosantos145@gmail.com>
2022-01-12 10:38:06 -0300

    um monte de coisa

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise-dog-image/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ViewDog from './components/ViewDog';
+
+function App() {
+  return (
+    <>
+      <ViewDog />
+    </>
+  );
+}
+
+export default App;
