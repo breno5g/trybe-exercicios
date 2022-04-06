d0002a1c742acdde65bc9af266a970b6583a3e90
breno5g <brenosantos145@gmail.com>
2022-04-06 11:06:37 -0300

    ex 02

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
@@ -1,3 +1,7 @@
 -- 1. Escreva uma query que exiba o maior salário da tabela. 
 SELECT MAX(SALARY) FROM hr.employees;
 
+-- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
+
+SELECT MAX(SALARY) - MIN(SALARY) FROM hr.employees;
+
