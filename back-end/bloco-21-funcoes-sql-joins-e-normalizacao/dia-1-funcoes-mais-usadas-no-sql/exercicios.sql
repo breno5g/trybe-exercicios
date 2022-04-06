003cf6c84bf6e93949a6ea728bb0b2cd5216e095
breno5g <brenosantos145@gmail.com>
2022-04-06 12:23:51 -0300

    ex 13

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
@@ -61,3 +61,7 @@ WHERE phone_number LIKE '515%';
 SELECT *
 FROM hr.employees
 WHERE LENGTH(first_name) >= 8;
+
+--  13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano). 
+
+SELECT EMPLOYEE_ID, FIRST_NAME, YEAR(HIRE_DATE) FROM hr.employees;
