455043e6d5070da1c5f829e09d994751d9d8a212
breno5g <brenosantos145@gmail.com>
2022-04-06 11:16:34 -0300

    ex 03

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
@@ -5,3 +5,7 @@ SELECT MAX(SALARY) FROM hr.employees;
 
 SELECT MAX(SALARY) - MIN(SALARY) FROM hr.employees;
 
+--  3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente. 
+SELECT JOB_ID, AVG(salary) AS media FROM hr.employees
+GROUP BY JOB_ID
+ORDER BY media DESC;
