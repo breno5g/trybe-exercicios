ae657e4b73f12b35e68167d4351b27e1fe82d578
breno5g <brenosantos145@gmail.com>
2022-04-06 11:16:53 -0300

    ex 05

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicios.sql
@@ -14,3 +14,5 @@ ORDER BY media DESC;
 
 SELECT CONCAT('A grana necessaria para pagar geral é: ', SUM(SALARY)) AS Soma FROM hr.employees;
 
+--  5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais. 
+SELECT ROUND(MAX(SALARY), 2), ROUND(MIN(SALARY), 2), ROUND(SUM(SALARY), 2), ROUND(AVG(SALARY), 2) FROM hr.employees;
