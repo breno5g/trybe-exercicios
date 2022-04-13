111d0f00fe8d2265181f1f983affdcb3e0c90e11
breno5g <brenosantos145@gmail.com>
2022-04-13 16:03:51 -0300

    ainda tô confuso mas tem saude

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/scripts.js
@@ -0,0 +1,27 @@
+const readline = require('readline-sync');
+
+const scripts = [
+  { name: 'imc', path: './imc.js' },
+  { name: 'vel', path: './vel.js' },
+  { name: 'sort', path: './sort.js' },
+];
+
+const message = scripts.map((scr, index) => `N-${index + 1} - ${scr.name}`);
+
+message.unshift('Escolha um numero para rodar os script: ');
+
+/*
+  stack overflow é lindo demais
+  https://stackoverflow.com/questions/9270409/explode-string-and-make-a-line-break-after-each-word
+*/
+
+const messageToShow = message.join('\n');
+
+console.log(messageToShow);
+
+const selectedOption = readline.questionInt(
+  'Insira o numero da opção selecionada: '
+);
+
+// Consegui rodar utilizando o require mas não achei conteudo sobre isso. *lembrar de revisar*
+require(scripts[selectedOption - 1].path);
