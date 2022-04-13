8f9eb413549a3d5278bcf8cf4603be3e05f343b2
breno5g <brenosantos145@gmail.com>
2022-04-13 16:33:58 -0300

    fatorial validation update

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/fatorial.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/fatorial.js
@@ -3,7 +3,10 @@ const { questionInt } = require('readline-sync');
 const fatorial = () => {
   const initial = questionInt('Insira o valor para ser calculado: ');
 
-  if (initial % 1 !== 0 || initial < 0) return false;
+  if (initial % 1 !== 0 || initial < 0) {
+    console.log("O numero inserido é invalido");
+    return false;
+  }
   let res = initial;
 
   for (let i = initial - 1; i > 0; i--) {
