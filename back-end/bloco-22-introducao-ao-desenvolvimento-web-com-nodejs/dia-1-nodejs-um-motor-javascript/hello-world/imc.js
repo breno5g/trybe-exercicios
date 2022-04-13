27a67131ff12413047aab5dd22efd2d688555d1b
breno5g <brenosantos145@gmail.com>
2022-04-13 09:38:31 -0300

    imc calculator update

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/imc.js
@@ -1,8 +1,25 @@
-const readline = require("readline-sync");
+const readline = require('readline-sync');
 
-const weight = readline.questionFloat("Qual é o seu peso (KG)? ");
-const height = readline.questionFloat("Qual é sua altura (Metros)? ");
+const weight = readline.questionFloat('Qual é o seu peso (KG)? ');
+const height = readline.questionFloat('Qual é sua altura (Metros)? ');
 
-const imc = weight / (height ** 2);
+const imc = weight / height ** 2;
 
-console.log(`Seu IMC é de ${imc.toFixed(2)}`);
+if (imc <= 18.5) {
+  console.log(`Seu IMC é de ${imc.toFixed(2)} | Abaixo do peso (magreza)`);
+}
+if (imc >= 18.5 && imc <= 24.9) {
+  console.log(`Seu IMC é de ${imc.toFixed(2)} | Peso normal`);
+}
+if (imc >= 24.9 && imc <= 29.9) {
+  console.log(`Seu IMC é de ${imc.toFixed(2)} | Acima do peso (sobrepeso)`);
+}
+if (imc >= 29.9 && imc <= 34.9) {
+  console.log(`Seu IMC é de ${imc.toFixed(2)} | Obesidade grau I`);
+}
+if (imc >= 34.9 && imc <= 39.9) {
+  console.log(`Seu IMC é de ${imc.toFixed(2)} | Obesidade grau II`);
+}
+if (imc >= 40) {
+  console.log(`Seu IMC é de ${imc.toFixed(2)} | Obesidade grau III e IV`);
+}
