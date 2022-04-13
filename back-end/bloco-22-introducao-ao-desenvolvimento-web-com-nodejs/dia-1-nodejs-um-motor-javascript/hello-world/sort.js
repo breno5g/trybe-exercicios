0777a50e42b1240166f29171d5b8bed1c5b8f089
breno5g <brenosantos145@gmail.com>
2022-04-13 15:44:29 -0300

    sorteio bonito - update

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/sort.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/sort.js
@@ -1,10 +1,21 @@
 const readline = require('readline-sync');
 
-const pitaco = readline.questionInt('Escolha um numero de 0 a 10: ');
-const sorted = Math.round(Math.random() * 10);
+const start = () => {
+  const pitaco = readline.questionInt('Escolha um numero de 0 a 10: ');
+  const sorted = Math.round(Math.random() * 10);
 
-if (pitaco === sorted) {
-  console.log(`Congratulations my friendo`);
-} else {
-  console.log(`miou, o numero sorteado foi ${sorted}`);
-}
+  if (pitaco === sorted) {
+    console.log(`Congratulations my friendo`);
+  } else {
+    console.log(`miou, o numero sorteado foi ${sorted}`);
+  }
+
+  const playAgain = readline.question('Deseja jogar novamente? (y OR n) ');
+  if (playAgain.toLowerCase() === 'y') {
+    start();
+  } else {
+    console.log('Okay, nem queria mesmo');
+  }
+};
+
+start();
