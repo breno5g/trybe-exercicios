76c99409a00ac354551260a62d52fe00d74ad88e
breno5g <brenosantos145@gmail.com>
2022-04-13 16:33:01 -0300

    fatorial

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/fatorial.js
@@ -0,0 +1,16 @@
+const { questionInt } = require('readline-sync');
+
+const fatorial = () => {
+  const initial = questionInt('Insira o valor para ser calculado: ');
+
+  if (initial % 1 !== 0 || initial < 0) return false;
+  let res = initial;
+
+  for (let i = initial - 1; i > 0; i--) {
+    res *= i;
+  }
+
+  console.log(`A fatorial de ${initial} é ${res}.`);
+};
+
+fatorial();
