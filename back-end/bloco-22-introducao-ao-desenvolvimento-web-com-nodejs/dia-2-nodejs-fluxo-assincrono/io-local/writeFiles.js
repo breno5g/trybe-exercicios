4b3272b48fcdbd10284b27e32e059cee61f673ff
breno5g <brenosantos145@gmail.com>
2022-04-14 11:10:20 -0300

    update

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFiles.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFiles.js
@@ -11,10 +11,20 @@ const fs = require('fs').promises;
 //   });
 
 // o append "acrescenta" ao final do arquivo
-fs.appendFile('./meu-arquivo.txt', '\n Meu textão')
+// fs.appendFile('./meu-arquivo.txt', '\n Meu textão')
+//   .then(() => {
+//     console.log('Arquivo escrito com sucesso!');
+//   })
+//   .catch((err) => {
+//     console.error(`Erro ao escrever o arquivo: ${err.message}`);
+//   });
+
+// A flag wx abre o arquivo para escrita **apenas** caso ele não exista. Caso o contrário, um erro será lançado
+fs.writeFile('./meu-arquivo.txt', 'Eu estive aqui :eyes:', { flag: 'wx' })
   .then(() => {
-    console.log('Arquivo escrito com sucesso!');
+    console.log('Arquivo salvo');
   })
   .catch((err) => {
-    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+    // Se o arquivo existir, um erro é retornado
+    console.error(err.message);
   });
