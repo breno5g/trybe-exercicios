d7d878155a5aae2ffa6fb14f05c1595ffab5cf4f
breno5g <brenosantos145@gmail.com>
2022-04-14 16:08:16 -0300

    ex 001

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/ex01.js
@@ -0,0 +1,23 @@
+const farofa = (a, b, c) => {
+  const res = new Promise((resolve, reject) => {
+    if (
+      typeof a !== 'number' ||
+      typeof b !== 'number' ||
+      typeof c !== 'number'
+    ) {
+      reject(new Error('Informe apenas números'));
+    }
+
+    const result = (a + b) * c;
+
+    if (result < 50) reject(new Error('Valor muito baixo'));
+
+    resolve(result);
+  });
+
+  return res;
+};
+
+farofa(10, 10, 10)
+  .then((resp) => console.log(resp))
+  .catch((err) => console.log(err.message));
