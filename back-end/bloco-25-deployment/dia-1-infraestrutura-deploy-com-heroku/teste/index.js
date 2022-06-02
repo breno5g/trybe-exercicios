124b6abe86109186adb4331e50d3dbfcc46b03db
Breno Santos <brenosantos145@gmail.com>
2022-06-02 09:58:57 -0300

    update

---
+++ back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/teste/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Farofinha de ovo');
+});
+
+app.listen(3000, (_req, _res) => {
+  console.log('Server on');
+});
