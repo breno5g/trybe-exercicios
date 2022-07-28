e340a7b660756ecc0a34a7f8eeffda5d46caa867
Breno Santos <brenosantos145@gmail.com>
2022-07-28 11:37:37 -0300

    :sparkles:

--- back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/guia-mongodb-com-nodejs-e-poo/src/models/Connection.ts
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/guia-mongodb-com-nodejs-e-poo/src/models/Connection.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin',
+    || 'mongodb://localhost:27017/glassesStore',
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/guia-mongodb-com-nodejs-e-poo/src/routes/frame.route.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import FrameController from '../controllers/Frame.controller';
+import FrameModel from '../models/Frame.model';
+import FrameService from '../services/Frame.service';
+
+const route = Router();
+
+const frame = new FrameModel();
+const frameService = new FrameService(frame);
+const frameController = new FrameController(frameService);
+
+route.post('/frame', (req, res) => frameController.create(req, res));
+route.get('/frame/:id', (req, res) => frameController.readOne(req, res));
+
+export default route;
